ff303df12bda962caaf368cf65dc0c9a3096ae15
jb <jb239812@ohio.edu>
2025-10-16 20:22:11 -0400

    prometheus to chapter 1 vkGuide equivalent

---
+++ src/engine.cpp
@@ -0,0 +1,291 @@
+#include "engine.h"
+
+#include <SDL.h>
+#include <SDL_vulkan.h>
+
+#include <vk_types.h>
+#include <vk_initializers.h>
+#include <vk_images.h>
+
+#include "VkBootstrap.h"
+#include <array>
+#include <thread>
+#include <chrono>
+
+//============================================================================================================================
+//============================================================================================================================
+// Initialization
+//============================================================================================================================
+void PrometheusInstance::Init () {
+	// initializing SDL
+	SDL_Init( SDL_INIT_VIDEO );
+	SDL_WindowFlags windowFlags = ( SDL_WindowFlags ) ( SDL_WINDOW_VULKAN );
+
+	window = SDL_CreateWindow(
+		"Prometheus",
+		SDL_WINDOWPOS_UNDEFINED,
+		SDL_WINDOWPOS_UNDEFINED,
+		windowExtent.width,
+		windowExtent.height,
+		windowFlags );
+
+	initVulkan();
+	initSwapchain();
+	initCommands();
+	initSyncStructures();
+
+	// everything went fine
+	isInitialized = true;
+}
+
+//============================================================================================================================
+// Draw
+//============================================================================================================================
+void PrometheusInstance::Draw () {
+	// wait until the gpu has finished rendering the last frame. Timeout of 1 second
+	VK_CHECK( vkWaitForFences( device, 1, &getCurrentFrame().renderFence, true, 1000000000));
+	VK_CHECK( vkResetFences( device, 1, &getCurrentFrame().renderFence));
+
+	//request image from the swapchain
+	uint32_t swapchainImageIndex;
+	VK_CHECK( vkAcquireNextImageKHR( device, swapchain, 1000000000, getCurrentFrame().swapchainSemaphore, nullptr, &swapchainImageIndex ) );
+
+	// Vulkan handles are aliased 64-bit pointers, basically shortens later code
+	VkCommandBuffer cmd = getCurrentFrame().mainCommandBuffer;
+
+	// because we've hit the fence, we are safe to reset the image buffer
+	VK_CHECK( vkResetCommandBuffer( cmd, 0 ) );
+
+	// begin the command buffer recording. We will use this command buffer exactly once, so we want to let vulkan know that
+	VkCommandBufferBeginInfo cmdBeginInfo = vkinit::command_buffer_begin_info( VK_COMMAND_BUFFER_USAGE_ONE_TIME_SUBMIT_BIT );
+
+	// start the command buffer recording
+	VK_CHECK( vkBeginCommandBuffer( cmd, &cmdBeginInfo ) );
+
+	// transition the swapchain image from undefined to general layout
+	vkutil::transition_image( cmd, swapchainImages[ swapchainImageIndex ], VK_IMAGE_LAYOUT_UNDEFINED, VK_IMAGE_LAYOUT_GENERAL );
+
+	// sinusoidal clear color pulsing
+	VkClearColorValue clearValue;
+	float flash = std::abs( std::sin( frameNumber / 120.f ) );
+	clearValue = { { 0.0f, 0.0f, flash, 1.0f } };
+
+	VkImageSubresourceRange clearRange = vkinit::image_subresource_range( VK_IMAGE_ASPECT_COLOR_BIT );
+
+	// clear image
+	vkCmdClearColorImage( cmd, swapchainImages[ swapchainImageIndex ], VK_IMAGE_LAYOUT_GENERAL, &clearValue, 1, &clearRange );
+
+	// transition the image from layout general to ready-for-swapchain-handoff
+	vkutil::transition_image( cmd, swapchainImages[ swapchainImageIndex ], VK_IMAGE_LAYOUT_GENERAL, VK_IMAGE_LAYOUT_PRESENT_SRC_KHR );
+
+	// Kill recording, and put it in "executable" state
+	VK_CHECK(vkEndCommandBuffer(cmd));
+
+	// before submitting to the queue, we need to specify the specific dependencies
+	// we want to wait on the presentSemaphore, signaled when the swapchain is ready
+	// we will signal the renderSemaphore, when rendering has finished
+	VkCommandBufferSubmitInfo cmdinfo = vkinit::command_buffer_submit_info(cmd);
+	VkSemaphoreSubmitInfo waitInfo = vkinit::semaphore_submit_info( VK_PIPELINE_STAGE_2_COLOR_ATTACHMENT_OUTPUT_BIT_KHR, getCurrentFrame().swapchainSemaphore );
+	VkSemaphoreSubmitInfo signalInfo = vkinit::semaphore_submit_info( VK_PIPELINE_STAGE_2_ALL_GRAPHICS_BIT, getCurrentFrame().renderSemaphore );
+
+	VkSubmitInfo2 submit = vkinit::submit_info( &cmdinfo, &signalInfo, &waitInfo );
+
+	// submit command buffer to the queue and execute it... renderFence will now block until it finishes
+	VK_CHECK( vkQueueSubmit2( graphicsQueue, 1, &submit, getCurrentFrame().renderFence ) );
+
+	// swapchain present to visible window...
+	VkPresentInfoKHR presentInfo = {};
+	presentInfo.sType = VK_STRUCTURE_TYPE_PRESENT_INFO_KHR;
+	presentInfo.pNext = nullptr;
+	presentInfo.pSwapchains = &swapchain;
+	presentInfo.swapchainCount = 1;
+	// wait on renderSemaphore, to tell when we are finished preparing the image
+	presentInfo.pWaitSemaphores = &getCurrentFrame().renderSemaphore;
+	presentInfo.waitSemaphoreCount = 1;
+	presentInfo.pImageIndices = &swapchainImageIndex;
+	VK_CHECK( vkQueuePresentKHR( graphicsQueue, &presentInfo ) );
+
+	//increase the number of frames drawn
+	frameNumber++;
+}
+
+
+//============================================================================================================================
+// Main Loop
+//============================================================================================================================
+void PrometheusInstance::MainLoop () {
+	SDL_Event e;
+
+	bool quit = false;
+
+	while ( !quit ) {
+		// event handling loop
+		while ( SDL_PollEvent( &e ) != 0 ) {
+			if ( e.type == SDL_QUIT ) {
+				quit = true;
+			}
+
+			if ( e.type == SDL_WINDOWEVENT ) {
+				if ( e.window.event == SDL_WINDOWEVENT_MINIMIZED ) {
+					stopRendering = true;
+				}
+				if ( e.window.event == SDL_WINDOWEVENT_RESTORED ) {
+					stopRendering = false;
+				}
+			}
+		}
+
+		// handling minimized application
+		if ( stopRendering ) {
+			// throttle the speed to avoid busy loop
+			std::this_thread::sleep_for( std::chrono::milliseconds( 100 ) );
+		} else {
+			// we're ready to draw the next frame
+			Draw();
+		}
+	}
+}
+
+//============================================================================================================================
+// Cleanup
+//============================================================================================================================
+void PrometheusInstance::ShutDown () {
+	// if we successfully made it through init
+	if ( isInitialized ) {
+		// make sure the gpu has stopped all work
+		vkDeviceWaitIdle( device );
+
+		// kill frameData
+		for ( int i = 0; i < FRAME_OVERLAP; i++ ) {
+			// killing the command pool implicitly kills the command buffers
+			vkDestroyCommandPool( device, frameData[ i ].commandPool, nullptr );
+
+			// destroy sync objects
+			vkDestroyFence( device, frameData[ i ].renderFence, nullptr );
+			vkDestroySemaphore( device, frameData[ i ].renderSemaphore, nullptr );
+			vkDestroySemaphore( device, frameData[ i ].swapchainSemaphore, nullptr );
+		}
+
+		// destroy remaining resources
+		destroySwapchain();
+		vkDestroySurfaceKHR( instance, surface, nullptr );
+		vkDestroyDevice( device, nullptr );
+		vkb::destroy_debug_utils_messenger( instance, debugMessenger );
+		vkDestroyInstance( instance, nullptr );
+		SDL_DestroyWindow( window );
+	}
+}
+
+//===========================================================================================================================
+// Helpers
+//===========================================================================================================================
+void PrometheusInstance::initVulkan () {
+	// make the vulkan instance, with basic debug features
+	vkb::InstanceBuilder builder;
+	auto inst_ret = builder.set_app_name( "Prometheus" )
+		.request_validation_layers( useValidationLayers )
+		.use_default_debug_messenger()
+		.require_api_version( 1, 3, 0 )
+		.build();
+
+	vkb::Instance vkb_inst = inst_ret.value();
+
+	//grab the instance
+	instance = vkb_inst.instance;
+	debugMessenger = vkb_inst.debug_messenger;
+
+	// create a surface to render to
+	SDL_Vulkan_CreateSurface( window, instance, &surface );
+
+	//vulkan 1.3 features
+	VkPhysicalDeviceVulkan13Features features13{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_3_FEATURES };
+	features13.dynamicRendering = true;
+	features13.synchronization2 = true;
+
+	//vulkan 1.2 features
+	VkPhysicalDeviceVulkan12Features features12{ .sType = VK_STRUCTURE_TYPE_PHYSICAL_DEVICE_VULKAN_1_2_FEATURES };
+	features12.bufferDeviceAddress = true;
+	features12.descriptorIndexing = true;
+
+	//use vkbootstrap to select a gpu.
+	//We want a gpu that can write to the SDL surface and supports vulkan 1.3 with the correct features
+	vkb::PhysicalDeviceSelector selector{ vkb_inst };
+	vkb::PhysicalDevice physicalDeviceSelect = selector
+		.set_minimum_version( 1, 3 )
+		.set_required_features_13( features13 )
+		.set_required_features_12( features12 )
+		.set_surface( surface )
+		.select()
+		.value();
+
+	//create the final vulkan device
+	vkb::DeviceBuilder deviceBuilder{ physicalDeviceSelect };
+	vkb::Device vkbDevice = deviceBuilder.build().value();
+
+	// Get the VkDevice handle used in the rest of a vulkan application
+	device = vkbDevice.device;
+	physicalDevice = physicalDeviceSelect.physical_device;
+
+	// use vkbootstrap to get a Graphics queue
+	graphicsQueue = vkbDevice.get_queue( vkb::QueueType::graphics ).value();
+	graphicsQueueFamilyIndex = vkbDevice.get_queue_index( vkb::QueueType::graphics ).value();
+}
+
+void PrometheusInstance::initSwapchain () {
+	createSwapchain( windowExtent.width, windowExtent.height );
+}
+
+void PrometheusInstance::initCommands () {
+	VkCommandPoolCreateInfo commandPoolInfo = vkinit::command_pool_create_info( graphicsQueueFamilyIndex, VK_COMMAND_POOL_CREATE_RESET_COMMAND_BUFFER_BIT);
+	for ( int i = 0; i < FRAME_OVERLAP; i++ ) {
+		// create a command pool allocator
+		VK_CHECK( vkCreateCommandPool( device, &commandPoolInfo, nullptr, &frameData[ i ].commandPool ) );
+
+		// and a command buffer from that command pool
+		VkCommandBufferAllocateInfo cmdAllocInfo = vkinit::command_buffer_allocate_info( frameData[ i ].commandPool, 1 );
+		VK_CHECK( vkAllocateCommandBuffers( device, &cmdAllocInfo, &frameData[ i ].mainCommandBuffer ) );
+	}
+}
+
+void PrometheusInstance::initSyncStructures () {
+	VkFenceCreateInfo fenceCreateInfo = vkinit::fence_create_info( VK_FENCE_CREATE_SIGNALED_BIT );
+	VkSemaphoreCreateInfo semaphoreCreateInfo = vkinit::semaphore_create_info();
+	for ( int i = 0; i < FRAME_OVERLAP; i++ ) {
+	// we need to create one fence ( frame end mark )
+		VK_CHECK( vkCreateFence( device, &fenceCreateInfo, nullptr, &frameData[ i ].renderFence ) );
+	// and two semaphores: swapchain image ready, and render finished
+		VK_CHECK( vkCreateSemaphore( device, &semaphoreCreateInfo, nullptr, &frameData[ i ].swapchainSemaphore ) );
+		VK_CHECK( vkCreateSemaphore( device, &semaphoreCreateInfo, nullptr, &frameData[ i ].renderSemaphore ) );
+	}
+}
+
+//===========================================================================================================================
+// swapchain helpers
+//===========================================================================================================================
+void PrometheusInstance::createSwapchain ( uint32_t w, uint32_t h ) {
+	vkb::SwapchainBuilder swapchainBuilder{ physicalDevice, device, surface };
+	swapchainImageFormat = VK_FORMAT_B8G8R8A8_UNORM;
+	vkb::Swapchain vkbSwapchain = swapchainBuilder
+		//.use_default_format_selection()
+		.set_desired_format( VkSurfaceFormatKHR{ .format = swapchainImageFormat, .colorSpace = VK_COLOR_SPACE_SRGB_NONLINEAR_KHR } )
+		//use vsync present mode
+		.set_desired_present_mode( VK_PRESENT_MODE_FIFO_KHR )
+		.set_desired_extent( w, h )
+		.add_image_usage_flags( VK_IMAGE_USAGE_TRANSFER_DST_BIT )
+		.build()
+		.value();
+
+	//store swapchain and its related images
+	swapchain = vkbSwapchain.swapchain;
+	swapchainExtent = vkbSwapchain.extent;
+	swapchainImages = vkbSwapchain.get_images().value();
+	swapchainImageViews = vkbSwapchain.get_image_views().value();
+}
+
+void PrometheusInstance::destroySwapchain () {
+	vkDestroySwapchainKHR( device, swapchain, nullptr );
+	for (int i = 0; i < swapchainImageViews.size(); i++ ) {
+		// we are only destroying the imageViews, the images are owned by the OS
+		vkDestroyImageView( device, swapchainImageViews[ i ], nullptr );
+	}
+}
---
+++ src/engine.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <iostream>
+#include <chrono>
+#include <thread>
+
+#include <vk_types.h>
+
+struct frameData_t {
+	// frame sync primitives
+	VkSemaphore swapchainSemaphore;
+	VkSemaphore renderSemaphore;
+	VkFence renderFence;
+
+	// command buffer + allocator
+	VkCommandPool commandPool;
+	VkCommandBuffer mainCommandBuffer;
+};
+
+constexpr unsigned int FRAME_OVERLAP = 2;
+constexpr bool useValidationLayers = true;
+
+class PrometheusInstance {
+public:
+
+	bool isInitialized { false };
+	bool stopRendering { false };
+	int frameNumber { 0 };
+
+	// basic Vulkan necessities, environmental handles
+	VkInstance instance;						// Vulkan library handle
+	VkDebugUtilsMessengerEXT debugMessenger;	// debug output messenger
+	VkPhysicalDevice physicalDevice;			// GPU handle for the physical device in use
+	VkDevice device;							// the abstract device that we interact with
+	VkSurfaceKHR surface;						// the Vulkan window surface
+
+	// our frameData struct, which contains command pool/buffer + sync primitive handles
+	frameData_t frameData[ FRAME_OVERLAP ];
+	frameData_t& getCurrentFrame () { return frameData[ frameNumber % FRAME_OVERLAP ]; }
+
+	// the queue that we submit work to
+	VkQueue graphicsQueue;
+	uint32_t graphicsQueueFamilyIndex;
+
+	// window size, swapchain size
+	VkExtent2D windowExtent { 1700 , 900 };
+	VkExtent2D swapchainExtent;
+
+	// swapchain handles
+	VkSwapchainKHR swapchain;
+	VkFormat swapchainImageFormat;
+	std::vector< VkImage > swapchainImages;
+	std::vector< VkImageView > swapchainImageViews;
+
+	struct SDL_Window* window{ nullptr };
+	static PrometheusInstance& Get ();
+
+	void Init ();
+	void Draw ();
+	void MainLoop ();
+	void ShutDown ();
+
+private:
+	// init helpers
+	void initVulkan ();
+	void initSwapchain ();
+	void initCommands ();
+	void initSyncStructures ();
+
+	// swapchain helpers
+	void createSwapchain( uint32_t w, uint32_t h );
+	void destroySwapchain();
+};
