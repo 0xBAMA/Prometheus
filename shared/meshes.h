812a7ad570284f0d379c6a66f9677730e588044c
jb <jb239812@ohio.edu>
2026-06-08 23:04:31 -0400

    missing struct

--- shared/meshes.h
+++ shared/meshes.h
@@ -1,5 +1,13 @@
 #include <vk_types.h>
 
+struct Vertex {
+	glm::vec3 position;
+	float uv_x;
+	glm::vec3 normal;
+	float uv_y;
+	glm::vec4 color;
+};
+
 extern uint32_t Cube_vtx_count;
 extern Vertex Cube_vtx[];
 extern uint32_t Cube_idx_count;
